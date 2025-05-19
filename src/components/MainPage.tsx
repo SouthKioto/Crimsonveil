f7ef2c491959be97c8858d975f31479cebbe3c50
Bartłomiej <bolisegabartek6@gmail.com>
2025-05-19 18:24:34 +0200

    first commit

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -12,7 +12,6 @@ export const MainPage = () => {
   return (
     <>
       <h1>Main page</h1>
-
       <h2>Siema 2</h2>
       <button className="bg-red-500" onClick={addOne}>counter: {counter}</button>
     </>
