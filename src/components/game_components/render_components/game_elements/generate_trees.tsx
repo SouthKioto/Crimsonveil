1c8ed787c1dbb648132814c87fb81a2f714c36f2
Bartłomiej <bolisegabartek6@gmail.com>
2025-05-24 01:46:36 +0200

    Add generate trees method and Tree and Player classes

---
+++ src/components/game_components/render_components/game_elements/generate_trees.tsx
@@ -0,0 +1,42 @@
+import { getWidth, getHeight } from "../utils/max_width_and_height"
+import { Tree } from "../Objects/Tree"
+
+
+export const generate_trees = (countStructures: number, scene) => {
+
+  let tree;
+
+  const treeWidth = 64;
+  const treeHeight = 112;
+
+  const placedTrees: { x: number, y: number }[] = [];
+
+  for (let i = 1; i <= countStructures; i++) {
+    let randomX = Math.random() * getWidth();
+    let randomY = Math.random() * getHeight();
+
+    let randomTreeStage = Math.floor(Math.random() * 16);
+
+    if ([0, 1, 4, 5, 8, 9, 12, 13].includes(randomTreeStage)) {
+      i--;
+      continue;
+
+    }
+
+    const overlaps = placedTrees.some(tree => {
+      const dx = Math.abs(tree.x - randomX);
+      const dy = Math.abs(tree.y - randomY);
+      return dx < treeWidth && dy < treeHeight;
+    });
+
+    if (overlaps) continue;
+
+    tree = new Tree(scene, randomX, randomY, randomTreeStage);
+    tree.setScale(2)
+    scene.physics.add.existing(tree)
+    scene.add.existing(tree);
+
+    placedTrees.push({ x: randomX, y: randomY });
+
+  }
+}
--- src/components/game_components/render_components/scenes/Game.tsx
+++ src/components/game_components/render_components/scenes/Game.tsx
@@ -1,16 +1,23 @@
 import { EventBus } from "../EventBus";
+import { generate_trees } from "../game_elements/generate_trees.tsx";
+import { generate_map } from "../game_elements/generate_map.tsx";
 import { Scene } from "phaser";
-import { generate_map } from "../utils/generate_map";
 import { Player } from "../game_elements/Player";
+import { Tree } from "../Objects/Tree.tsx";
 
-var player: Phaser.Physics.Arcade.Sprite;
-var keys;
+var player: Player;
+var tree;
+var treeGroup;
 
 // creacte WSAD keys
-let KeyA;
-let KeyD;
-let KeyW;
-let KeyS;
+let KeyA: Phaser.Input.Keyboard.Key;
+let KeyD: Phaser.Input.Keyboard.Key;
+let KeyW: Phaser.Input.Keyboard.Key;
+let KeyS: Phaser.Input.Keyboard.Key;
+let KeySpaceAttack: Phaser.Input.Keyboard.Key;
+
+//movement
+const movement_speed = 200;
 
 export class Game extends Scene {
 
@@ -21,27 +28,45 @@ export class Game extends Scene {
   preload() {
 
     this.load.image("tiles", "public/assets/game_assets/TileSet_V1.png");
-    this.load.spritesheet("player", "public/assets/game_assets/player.png", {
+
+    this.load.spritesheet("player", "/assets/game_assets/player.png", {
       frameWidth: 100,
       frameHeight: 100,
+    });
+
+    this.load.spritesheet('tree', "/assets/game_assets/trees.png", {
+      frameWidth: 64,
+      frameHeight: 112,
     });
   }
 
   create() {
+    //keyMaps
     KeyA = this.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     KeyD = this.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     KeyW = this.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.W);
     KeyS = this.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    KeySpaceAttack = this.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
 
-
+    //generate map
     const array: number[][] = generate_map(100, 100);
     const map = this.make.tilemap({ data: array, tileWidth: 32, tileHeight: 32 })
     const tiles = map.addTilesetImage(null, "tiles");
     const layer = map.createLayer(0, tiles, 0, 0)
 
-    player = this.physics.add.sprite(500, 500, 'player');
 
-    keys = this.input.keyboard?.createCursorKeys();
+    //generate trees
+    generate_trees(300, this)
+
+    //adding player
+    const playerSprite = new Player(this, 400, 400);
+    player = this.physics.add.existing(playerSprite) as Player;
+    this.add.existing(player);
+    player.setScale(2)
+
+    this.physics.add.collider(player, treeGroup)
+
+    //keys = this.input.keyboard?.createCursorKeys();
 
     this.anims.create({
       key: "idle",
@@ -57,39 +82,48 @@ export class Game extends Scene {
       repeat: -1
     })
 
-    //    EventBus.emit('current-scene-ready', this);
+    this.anims.create({
+      key: 'attack',
+      frames: this.anims.generateFrameNumbers('player', { start: 18, end: 23 }),
+      frameRate: 10,
+      repeat: 1
+    })
+
+    //EventBus.emit('current-scene-ready', this); //potrzebne do laczenia phasera i react ui
   }
 
   update() {
+    if (!player || !player.body) return;
 
     var isMoving = false;
+    var isAttacking = false;
 
     player.setVelocity(0)
 
-
     if (KeyA.isDown) {
-      player.setVelocityX(-100)
+      player.setVelocityX((-1) * (movement_speed))
     } else if (KeyD.isDown) {
-      player.setVelocityX(100)
+      player.setVelocityX(movement_speed)
 
     } else if (KeyW.isDown) {
-      player.setVelocityY(-100)
+      player.setVelocityY((-1) * (movement_speed))
 
     } else if (KeyS.isDown) {
-      player.setVelocityY(100)
+      player.setVelocityY(movement_speed)
     }
 
-    if (!isMoving) {
+    if (!isMoving && isAttacking) {
       if (player.anims.currentAnim?.key !== 'walking') {
         player.play('walking');
       }
-
     } else {
       if (player.anims.currentAnim?.key !== 'idle') {
         player.play("idle");
       }
     }
 
+    if (KeySpaceAttack.isDown) {
+      player.play("attack")
+    }
   }
-
 }
