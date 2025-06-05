378ff168d98490331049473d4b31111785d7717a
Bartłomiej <bolisegabartek6@gmail.com>
2025-06-05 23:29:09 +0200

    add render equipment table (to repair)

--- src/components/ui/MainPage.tsx
+++ src/components/ui/MainPage.tsx
@@ -1,54 +1,33 @@
-import { useState, useEffect } from "react"
-import { NavLink } from "react-router-dom"
-import { Login } from "./Login/Login"
-
-type Wife = {
-  name: string,
-  surname: string,
-}
-
-type Person = {
-  name: string,
-  surname: string,
-  wife: Wife;
-}
-
-
+import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
 export const MainPage = () => {
-
   const [counter, setCounter] = useState(0);
 
-  const obj: Person = {
-    name: 'name',
-    surname: 'surname',
-    wife: {
-      name: 'wife_name',
-      surname: 'wife_surname'
-    }
-  }
-
   const addOne = () => {
-    setCounter(counter + 1)
-  }
+    setCounter(counter + 1);
+  };
 
   return (
     <>
       <h1>Main page</h1>
-      <button className="bg-red-500 rounded-2xl p-2" onClick={addOne}>counter: {counter}</button>
+      <button className="bg-red-500 rounded-2xl p-2" onClick={addOne}>
+        counter: {counter}
+      </button>
 
       <br></br>
       <NavLink to={"/game"}>
-        <button className="bg-blue-700 rounded-2xl p-3 hover:bg-gradient-to-r from-pink-500-500 from-10% via-white via-30% to-orange-500 to-90% transition delay-150 duration-300 ease-in-out hover:border-5 border-black">Siema to jest button sluzacy do przejscia do gry XD</button>
+        <button className="bg-blue-700 rounded-2xl p-3 hover:bg-gradient-to-r from-pink-500-500 from-10% via-white via-30% to-orange-500 to-90% transition delay-150 duration-300 ease-in-out hover:border-5 border-black">
+          Siema to jest button sluzacy do przejscia do gry XD
+        </button>
       </NavLink>
 
-      <h1>{obj.name}, {obj.surname}, {obj.wife.name}</h1>
-
-      <button>Siema</button>
-
-      <Login />
+      <br></br>
+      <NavLink to={"/game-ui"}>
+        <button className="bg-blue-700 rounded-2xl p-3 hover:bg-gradient-to-r from-pink-500-500 from-10% via-white via-30% to-orange-500 to-90% transition delay-150 duration-300 ease-in-out hover:border-5 border-black">
+          Game ui
+        </button>
+      </NavLink>
     </>
-
-  )
-}
-
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { MainPage } from './components/ui/MainPage'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { MainPage } from "./components/ui/MainPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { GamePage } from './components/ui/GamePage.tsx';
+import { GamePage } from "./components/ui/GamePage.tsx";
+import { Game_Ui } from "./components/react-game-ui/Game_Ui.tsx";
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<MainPage />} />
+        <Route path="/" element={<MainPage />} />
 
-        <Route path='/game' element={<GamePage />} />
+        <Route path="/game" element={<GamePage />} />
+
+        <Route path="/game-ui" element={<Game_Ui />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>
-)
+  </StrictMode>,
+);
