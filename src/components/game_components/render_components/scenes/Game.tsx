d486d597e78df9cfd49066b31f6d2bb27b5a48c2
Bartłomiej <bolisegabartek6@gmail.com>
2025-06-09 14:59:39 +0200

    naprawa generacji chunkow

--- src/components/game_components/render_components/scenes/Game.tsx
+++ src/components/game_components/render_components/scenes/Game.tsx
@@ -1,18 +1,16 @@
-import { EventBus } from "../EventBus";
-import { generate_trees } from "../game_elements/generate_trees.tsx";
 import { generate_chunks } from "../game_elements/generate_chunks.tsx";
+import { generate_frogs } from "../game_elements/generate_frogs.tsx";
+import { generate_trees } from "../game_elements/generate_trees.tsx";
+
+import { EventBus } from "../EventBus.tsx";
 import { Scene } from "phaser";
 import { Player } from "../Objects/Characters/Player";
 import { Tree } from "../Objects/Environment/Tree.tsx";
 import { Frog } from "../Objects/Enemies/Frog.tsx";
 import { Sword } from "../Objects/Weapons/Sword.tsx";
 import { Equipment } from "../Objects/Ui/Equipment.tsx";
-import { generate_frogs } from "../game_elements/generate_frogs.tsx";
 import { CreateFrogAnims } from "../anims/CreateFrogAnims.tsx";
 import { CreatePlayerAnims } from "../anims/CreatePlayerAnims.tsx";
-import { add_player } from "../game_elements/add_player.tsx";
-import { searchForWorkspaceRoot } from "vite";
-import { Enemy } from "../Objects/Enemies/Enemy.tsx";
 
 export class Game extends Scene {
   private player: Player;
@@ -127,8 +125,6 @@ export class Game extends Scene {
     //frog = new Frog(this, 500, 700, 'frog_blueblue', 'frog_hop', 'frog_stay', 'frog_attack')
     //console.log(`frog health: ${frog.health}`)
 
-    generate_frogs(this, 5);
-
     //adding player
     this.player = new Player(this, 500, 500, "player");
     this.cameras.main.startFollow(this.player);
@@ -147,24 +143,24 @@ export class Game extends Scene {
     }
 
     console.log(this.player.showEquipment());
-
-    //generate trees
-    generate_trees(50, this);
-
     //this.physics.add.collider(player, treeGroup)
-
-    //EventBus.emit('current-scene-ready', this); //potrzebne do laczenia phasera i react ui
   }
 
   update() {
     this.player.update();
     const playerChunkX = Math.floor(this.player.x / this.chunkSize);
     const playerChunkY = Math.floor(this.player.y / this.chunkSize);
+    let chunks: { x: number; y: number }[] = [];
 
     const chunkKey = `${playerChunkX}, ${playerChunkY}`;
     if (!this.generatedChunks.has(chunkKey)) {
       generate_chunks(this, playerChunkX, playerChunkY, this.chunkSize);
       this.generatedChunks.add(chunkKey);
+      let chunkPos = { playerChunkX, playerChunkY };
+      chunks.push(chunkPos);
+
+      generate_frogs(this, 5);
+      generate_trees(this, 20, this.chunkSize);
     }
   }
 }
