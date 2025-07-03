53cd3ba95b778774297dbbd3a2bf16a317905c9f
Bartłomiej <bolisegabartek6@gmail.com>
2025-07-03 16:41:43 +0200

    create base navbar

--- src/components/ui/MainPage.tsx
+++ src/components/ui/MainPage.tsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import { Navbar } from "./elements/Navbar";
 
 export const MainPage = () => {
   return (
     <>
+      <Navbar />
       <NavLink to={"/game"}>
         <button className="bg-blue-700 rounded-2xl p-3 hover:bg-gradient-to-r from-pink-500-500 from-10% via-white via-30% to-orange-500 to-90% transition delay-150 duration-300 ease-in-out hover:border-5 border-black">
           Siema to jest button sluzacy do przejscia do gry XD
--- src/components/ui/elements/Navbar.tsx
+++ src/components/ui/elements/Navbar.tsx
@@ -1,7 +1,25 @@
 export const Navbar = () => {
   return (
     <>
-      <p>Navbar</p>
+      <header>
+        <nav className="bg-gray-800">
+          <div className="nav-left">
+            <div className="logo">
+              <img src="public/assets/ui_assets/crimsonveil_150x150.png" />
+            </div>
+          </div>
+
+          <div className="nav-center">
+            <ul className="nav-links">
+              <li>Main Page</li>
+              <li>About Game</li>
+              <li>Support</li>
+            </ul>
+          </div>
+
+          <div className="nav-right"></div>
+        </nav>
+      </header>
     </>
   );
 };
