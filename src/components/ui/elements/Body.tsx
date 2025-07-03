ed276cdd98f0f4ec6d0b08a001f56874a3174a21
Bartłomiej <bolisegabartek6@gmail.com>
2025-07-03 20:31:08 +0200

    add body component for main page

--- src/components/ui/elements/Body.tsx
+++ src/components/ui/elements/Body.tsx
@@ -1,8 +1,18 @@
 export const Body = () => {
   return (
     <>
-      <div className="h-200 m-auto bg-black">
+      <div className="h-500 w-350 m-auto bg-gray-800">
         <p>Body</p>
+        <p>
+          Playlista do hardo SEXO przy projekcie <br></br>
+          <a
+            target="_blank"
+            className="text-red-500"
+            href="https://music.youtube.com/playlist?list=PL0vQUhMIMX3hd5j0LaZ_GLO2quzh_PThc&si=3QVYq4EbaLJeP7Al"
+          >
+            hardo sexo
+          </a>
+        </p>
       </div>
     </>
   );
